cb51010f2940036edd5f381f6425677f8cd834ac
chezzzer <46093784+chezzzer@users.noreply.github.com>
2022-01-06 19:16:26 +1300

    update

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+page('*', (ctx) => {
+    page.currentPage = ctx;
+    let path = ctx.path == "/" ? "home" : ctx.path;
+
+    $.ajax({
+        url: `/pages/${path}.html`,
+        success: (content) => {
+            $("#content").html(content);
+            $(".header-link").removeClass("active");
+            $(`.header-link[href='${ctx.path}']`).addClass("active");
+        },
+        error: (e) => {
+            alert(`${e.status} ${e.statusText}`);
+            page.back();
+        }
+    })
+})
+
+page({
+    hashbang:true
+})
+
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset == 0) {
+        $("header").removeClass("active")
+    } else {
+        $("header").addClass("active")
+    }
+});
